c7dfed962705e8f7d7569d4b67eb2ea1a83c32a7
Allan Imire <allanimire@gmail.com>
2022-08-26 10:56:55 +0300

    Modify Store Models & Add Migrations

--- store/models.py
+++ store/models.py
@@ -11,7 +11,8 @@ class Collection(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    slug = models.SlugField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -38,6 +39,7 @@ class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    zip = models.CharField(max_length=255, null=True)
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
