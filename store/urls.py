75c91a9e0cb4a898471a0f34335551ca7f69d92f
Allan Imire <allanimire@gmail.com>
2022-09-20 22:19:09 +0300

    create API view and Serializer classes

--- store/urls.py
+++ store/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('create-customer/', views.create_customer, name='create-customer'),
-    path('json/', views.json, name='json'),
-    path('template-view/', views.ExampleTemplateView.as_view(), name='json'),
+    path('products/', views.product_list, name='products-list'),
+    path('products/<int:id>/', views.product_detail, name='products-detail'),
+    path('collections/<int:pk>/', views.collection_detail, name='collection-detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,75 +1,27 @@
-from django.db.models.aggregates import Count, Min, Max, Avg, Sum
-from django.db.models import F, Func, Value
-from django.db.models.functions import Concat
-from django.http import HttpResponse, HttpResponseServerError, JsonResponse
-from django.shortcuts import render
-from django.views.generic import TemplateView
-from store.models import Product, Customer, Collection, Product, Order, OrderItem
+from multiprocessing import context
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-def index(request):
-    # queryset = Customer.objects.filter(email__icontains='.com')
-    # queryset = Collection.objects.filter(featured_product__isnull=True)
-    # queryset = Product.objects.filter(inventory__lt=10)
-    # queryset = Order.objects.filter(customer_id=1)
-    # queryset = OrderItem.objects.filter(product__collection_id=3)
-    # queryset = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
-    # queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
-    # orders = Order.objects.aggregate(count=Count('id'))
-    # product_1 = OrderItem.objects.filter(product_id=1).aggregate(units_sold=Sum('quantity'))
-    # customer_1 = Order.objects.filter(customer_id=1).aggregate(count=Count('id'))
-    # price = Product.objects.filter(collection_id=3).aggregate(min=Min('unit_price'), max=Max('unit_price'), average=Avg('unit_price'))
-    # queryset = Customer.objects.annotate(is_new=True)
-    # queryset = Customer.objects.annotate(
-    #     full_name=Func(F('first_name'),Value(' '),F('last_name'),function='CONCAT')
-    # )
-    # queryset = Customer.objects.annotate(
-    #     full_name=Concat('first_name', Value(' '), 'last_name')
-    # )
-    # customers = Customer.objects.annotate(
-    #     last_order_id=Max('order__id')
-    # )
-    # collections = Collection.objects.annotate(
-    #     product_counts=Count('product')
-    # )
-    # customers = Customer.objects.annotate(
-    #     orders_count=Count('order')
-    # ).filter(orders_count__gt=5)
-    customers = Customer.objects.annotate(
-        total_spend=Sum(
-            F('order__orderitem__unit_price') *
-            F('order__orderitem__quantity') 
-        )
-    )
-    list(customers)
-    return render(request, 'index.html', {})
+from .models import Collection, Product
+from .serializers import CollectionSerializer, ProductSerializer
 
-def create_customer(request):
-    if request.method == 'POST':
-        customer = {
-            'first_name': request.POST.get('first_name', None),
-            'last_name': request.POST.get('last_name', None),
-            'email':request.POST.get('email', None),
-            'phone': request.POST.get('phone', None),
-            'membership': request.POST.get('membership', None),
-            'birth_date': request.POST.get('birth_date', None)
-        }
-        response_message = None
-        try:
-            customer = Customer(**customer)
-            customer.save()
-            response_message = f'Customer {customer.first_name} has been created!'
-        except Exception as e:
-            return HttpResponseServerError(e)
-        return HttpResponse(response_message)
 
-def json(request):
-    product_list = list(Product.objects.filter(collection_id=5).values())
-    return JsonResponse(product_list, safe=False)
+@api_view()
+def product_list(request):
+    products = Product.objects.select_related('collection').all()
+    serializer = ProductSerializer(products, many=True, context={'request': request})
+    return Response(serializer.data)
 
-class ExampleTemplateView(TemplateView):
-    template_name = 'template-view.html'
+@api_view()
+def product_detail(request, id):
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'This is a template view'
-        return context
+@api_view()
+def collection_detail(request, pk):
+    collection = get_object_or_404(Collection, pk=pk)
+    serializer = CollectionSerializer(collection)
+    return Response(serializer.data)
